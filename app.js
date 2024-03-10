10764dc67ec59dc6284adb529482dc008e088e94
tbert31 <berteau.thomas@hotmail.fr>
2024-03-10 15:07:17 +0100

    allow origin

--- app.js
+++ app.js
@@ -23,7 +23,12 @@ app.use(express.json());
 // Peux se résoudre avec le package cors app.use(cors); 
 // pour solutionner le problème de sécurité côté browser 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Permet d'accéder à notre API depuis seulement l'url du frontend, mieu que * pour des raisons de sécurité !
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Permet d'accéder à notre API depuis seulement l'url du frontend, mieu que * pour des raisons de sécurité !
+    const allowedOrigins = ['http://localhost:3000', 'https://thomas-berteau.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // Permet d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Permet d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.)
     next();
